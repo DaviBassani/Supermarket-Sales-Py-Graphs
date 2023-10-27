ce671a0821a098929213efc6f002dd9ce2b9c248
Davi Bassani <bassani.davi@gmail.com>
2023-10-27 17:49:15 -0300

    Code refactored

--- dash.py
+++ dash.py
@@ -2,45 +2,64 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(layout="wide")
+def load_data():
+    # Carrega os dados do arquivo CSV
+    df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
+    df["Date"] = pd.to_datetime(df["Date"])
+    df = df.sort_values("Date")
+    return df
 
-# Faturamento por unidade
-# Tipo de produto mais vendido
-# Desempenho da forma de pagamento
-# Como estão as avaliações filiais?
+def filter_data_by_month(df, selected_month):
+    # Filtra os dados com base no mês selecionado
+    return df[df["Month"] == selected_month]
 
-df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
-df["Date"] = pd.to_datetime(df["Date"])
-df =  df.sort_values("Date")
+def generate_revenue_by_day_chart(df_filtered):
+    # Gera um gráfico de barras de faturamento por dia
+    fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Faturamento por dia")
+    return fig_date
 
-df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+def generate_revenue_by_product_chart(df_filtered):
+    # Gera um gráfico de barras horizontais de faturamento por tipo de produto
+    fig_prod = px.bar(df_filtered, x="Date", y="Product line", color="City", orientation="h", title="Faturamento por tipo de produto")
+    return fig_prod
 
-month = st.sidebar.selectbox("Mês", df["Month"].unique())
+def generate_revenue_by_branch_chart(df_filtered):
+    # Gera um gráfico de barras de faturamento por filial
+    city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
+    fig_city = px.bar(city_total, x="City", y="Total", title="Faturamento por filial")
+    return fig_city
 
-df_filtered = df[df["Month"] == month]
+def generate_revenue_by_payment_type_chart(df_filtered):
+    # Gera um gráfico de pizza do faturamento por tipo de pagamento
+    fig_kind = px.pie(df_filtered, values="Total", names="Payment", title="Faturamento por tipo de pagamento")
+    return fig_kind
 
-col1, col2 = st.columns(2)
-col3, col4, col5 = st.columns(3)
+def generate_branch_ratings_chart(df_filtered):
+    # Gera um gráfico de barras das avaliações das filiais
+    city_avg_rating = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
+    fig_rating = px.bar(city_avg_rating, x="City", y="Rating", title="Avaliação")
+    return fig_rating
 
-fig_date = px.bar(df_filtered, x="Date", y="Total",
-                    color="City", title="Faturamento por dia")
-col1.plotly_chart(fig_date)
+def main():
+    st.set_page_config(layout="wide")
+    
+    df = load_data()
+    
+    df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+    unique_months = df["Month"].unique()
+    
+    selected_month = st.sidebar.selectbox("Selecione um Mês", unique_months)
+    
+    df_filtered = filter_data_by_month(df, selected_month)
+    
+    col1, col2 = st.columns(2)
+    col3, col4, col5 = st.columns(3)
+    
+    col1.plotly_chart(generate_revenue_by_day_chart(df_filtered))
+    col2.plotly_chart(generate_revenue_by_product_chart(df_filtered))
+    col3.plotly_chart(generate_revenue_by_branch_chart(df_filtered))
+    col4.plotly_chart(generate_revenue_by_payment_type_chart(df_filtered), use_container_width=True)
+    col5.plotly_chart(generate_branch_ratings_chart(df_filtered), use_container_width=True)
 
-fig_prod = px.bar(df_filtered, x="Date", y="Product line",
-                    color="City", orientation="h", 
-                    title="Faturamento por tipo de produto")
-col2.plotly_chart(fig_prod)
-
-city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
-fig_city = px.bar(df_filtered, x="City",
-                    y="Total",title="Faturamento por filial")
-col3.plotly_chart(fig_city)
-
-fig_kind = px.pie(df_filtered, values="Total", names="Payment",
-                   title="Faturamento por tipo de pagamento")
-col4.plotly_chart(fig_kind, use_container_width=True)
-
-city_total = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
-fig_rating = px.bar(df_filtered, y="Rating", x="City",
-                   title="Avaliação")
-col5.plotly_chart(fig_rating, use_container_width=True)
+if __name__ == "__main__":
+    main()
